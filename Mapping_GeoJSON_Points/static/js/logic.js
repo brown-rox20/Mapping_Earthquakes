458fb3dc196ee8f8035683fd62c0571f5bac269e
Roxane Brown <roxahumada83@gmail.com>
2022-03-20 11:11:01 -0700

    finish 13.5.2 module work (GeoJSON data)

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,24 +1,45 @@
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([37.6213, -122.379], 5);
+// Add GeoJSON data.
+let sanFranAirport = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        id: "3469",
+        name: "San Francisco International Airport",
+        city: "San Francisco",
+        country: "United States",
+        faa: "SFO",
+        icao: "KSFO",
+        alt: "13",
+        "tz-offset": "-8",
+        dst: "A",
+        tz: "America/Los_Angeles",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-122.375, 37.61899948120117],
+      },
+    },
+  ],
+};
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.379],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088],
-];
+// Create the map object with center at the San Francisco airport.
+let map = L.map("mapid").setView([37.5, -122.5], 10);
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "yellow",
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function (feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+  },
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
