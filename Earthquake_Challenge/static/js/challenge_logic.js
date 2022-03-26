95620c984b9e611f141a50f56c569c04a40b5e5e
Roxane Brown <roxahumada83@gmail.com>
2022-03-26 13:22:50 -0700

    add tectonic plates to map

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -38,10 +38,12 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
+let allTectonicPlates = new L.LayerGroup();
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
   Earthquakes: allEarthquakes,
+  "Tectonic Plates": allTectonicPlates,
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -156,5 +158,13 @@ d3.json(
   legend.addTo(map);
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(function () {});
+  d3.json(
+    "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+  ).then(function (data) {
+    L.geoJSON(data, {
+      style: { color: "#FFA500", weight: 3 },
+    }).addTo(allTectonicPlates);
+  });
+
+  allTectonicPlates.addTo(map);
 });
